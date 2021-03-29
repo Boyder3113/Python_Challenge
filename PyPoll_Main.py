b91e26dae45218d9a8092ac3d70105788e269252
Boyd Evans III <bbeiii@yahoo.com>
2021-03-28 18:10:20 -0600

    changes to pypoll after meeting with tutor

--- PyPoll_Main.py
+++ PyPoll_Main.py
@@ -26,19 +26,39 @@ with open(election_csv) as csv_file:
            votes[row[2]] = votes[row[2]]+1
 
 
-if total_votes > votes_winner:
-    winning_votes[1] = votes
-    winning_votes[0] = row[2]
+#if total_votes > votes_winner:
+    #winning_votes[1] = votes
+    #winning_votes[0] = row[2]
 
 
-print("Election Results")
-print("-----------------------------------------------------")
-print(f"Total Votes: {total_votes}")
-print("-----------------------------------------------------")
-for people in votes:
-    print(f"{people}: {round(((votes[people]/total_votes)*100))}% ({votes[people]})") 
-print("-----------------------------------------------------")
-
+with open (output_path, 'w',) as output_file:
+    print("Election Results")
+    output_file.write("Election Results")
+    output_file.write("\n")
+    print("-----------------------------------------------------")
+    output_file.write("-----------------------------------------------------")
+    print(f"Total Votes: {total_votes}")
+    output_file.write("\n")
+    output_file.write("Total Votes: " + str(total_votes))
+    output_file.write("\n")
+    print("-----------------------------------------------------")
+    output_file.write("-----------------------------------------------------")
+    output_file.write("\n")
+    for people in votes:
+        vote = votes.get(people)
+        if vote > votes_winner:
+            votes_winner = vote
+            winning_votes[1] = vote
+            winning_votes[0] = people
+        print(f"{people}: {round(((vote/total_votes)*100))}% ({vote})")
+        output_file.write(str(people) + str((vote/total_votes)*100).format(0.00) + " (" + str(vote) + ")")
+        output_file.write("\n")
+    print("-----------------------------------------------------")
+    output_file.write("-----------------------------------------------------")
+    output_file.write("\n")
+    print(f"Winner: {winning_votes[0]}")
+    output_file.write("Winner: " + str(winning_votes[0]))
+    output_file.write("\n")
 #with open (output_path, 'w',) as output_file:
     #output_file.write("Election Results")
     #output_file.write("\n")
